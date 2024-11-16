23885bb8bd9e70d1d8dc7951360535d7d2063ee8
KevinLeleux <beholderer@gmail.com>
2024-11-16 12:56:43 +0100

    :wrench: chore: prisma, auth, user server controllers and service initialized

---
+++ apps/server/src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  registerUser(@Body() createUserDto: CreateUserDto) {}
+}
